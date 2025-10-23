f47bdef29039e13f92190ab06c7b91416a8e64f5
anthonyamrm <anthonyamorim02@gmail.com>
2025-10-23 04:31:02 -0300

    anthony#25 logout

--- Rec/reciclAI/urls.py
+++ Rec/reciclAI/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage,name='homepage'),
+    path('', views.homepage,name=''),
     path('cadastro/', views.cadastro,name='cadastro'),
     path('login/', views.login,name='login'),
-    path('dashboard-reciclador', views.dashboard_reciclador,name='reciclador'),
-    path('dashboard-gerador', views.dashboard_reciclador,name='gerador'),
+    path('dashboard-reciclador/', views.dashboard_reciclador,name='reciclador'),
+    path('dashboard-gerador/', views.dashboard_reciclador,name='gerador'),
+    path('logout/', views.logout,name='logout'),
 ]
 
