f89c1d508e6be7325fbfc96f5eaa86cc4e1f4b51
anthonyamrm <anthonyamorim02@gmail.com>
2025-10-23 03:03:09 -0300

    anthonyamrm#31 registro de usuario

--- Rec/reciclAI/views.py
+++ Rec/reciclAI/views.py
@@ -1,4 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import CustomUserCreationForm
+from .models import UserProfile 
 
 def homepage(request):
     return render(request, 'reciclAI/homepage.html')
+
+def cadastro(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+
+        if form.is_valid():
+            
+            user = form.save()
+
+            cpf_cnpj_data = form.cleaned_data.get('cpf_cnpj')
+            telefone_data = form.cleaned_data.get('telefone')
+            
+            tipo_usuario_data = form.cleaned_data.get('tipo_usuario', None) 
+            profile = UserProfile.objects.create(
+                user=user,
+                cpf_cnpj=cpf_cnpj_data,
+                telefone=telefone_data,
+                tipo_usuario=tipo_usuario_data if tipo_usuario_data else 'GERADOR',
+          
+            )
+            
+            return redirect('homepage')        
+        
+    else:
+        form = CustomUserCreationForm()
+    
+    context = {'cadastro_form': form}
+    return render(request, 'reciclAI/cadastro.html', context)
