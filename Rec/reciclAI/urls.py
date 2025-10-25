9c1005a42fdce8b73ce3d75597612c8f97a6ee70
guidesiderio <guilhermedesiderio23@gmail.com>
2025-10-25 20:53:36 -0300

    Refatora URLs e funções de visualização: corrige nomes de funções e redirecionamentos

--- Rec/reciclAI/urls.py
+++ Rec/reciclAI/urls.py
@@ -2,13 +2,13 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.homepage,name=''),
-    path('cadastro/', views.cadastro,name='cadastro'),
-    path('login/', views.login,name='login'),
-    path('dashboard-reciclador/', views.dashboard_reciclador,name='reciclador'),
-    path('dashboard-gerador/', views.dashboard_reciclador,name='gerador'),
-    path('logout/', views.logout,name='logout'),
-    path('registro-material/', views.registrar_material,name='registro-material'),
-]
 
+urlpatterns = [
+    path('', views.homepage, name='home'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('login/', views.login_view, name='login'),
+    path('dashboard-reciclador/', views.dashboard_reciclador, name='reciclador'),
+    path('dashboard-gerador/', views.dashboard_gerador, name='gerador'),
+    path('logout/', views.logout_view, name='logout'),
+    path('registro-material/', views.registrar_material, name='registro-material'),
+]
