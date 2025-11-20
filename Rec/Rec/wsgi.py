e9d6f1ebb8d116aa631594d0a5d7469ab8fafbd6
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2025-11-19 21:25:33 -0300

    Feat/dashboards #42 (#63)
    * Criar dashboards e redirecionamentos por tipo de usuário
    Fixes #42
    
    * Commitando na Branch correta

--- Rec/Rec/wsgi.py
+++ Rec/Rec/wsgi.py
@@ -1,8 +1,7 @@
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Rec.settings")
 
 application = get_wsgi_application()
--- Rec/reciclAI/apps.py
+++ Rec/reciclAI/apps.py
@@ -3,8 +3,8 @@ from django.apps import AppConfig
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "core"
+    name = "reciclAI"
 
     def ready(self):
         # Import signals to ensure they are registered when the app is ready
-        import core.signals  # noqa: F401
+        import reciclAI.signals  # noqa: F401
