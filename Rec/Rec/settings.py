436a59fce6256ec02c1fa38c8dacd274779013ba
anthonyamrm <anthonyamorim02@gmail.com>
2025-11-26 17:50:20 -0300

    conf production -> gunicorn e settings

--- Rec/Rec/settings.py
+++ Rec/Rec/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get(
 
 # DEBUG should be disabled in production; set DJANGO_DEBUG env var to
 # 'False' there
-DEBUG = os.environ.get("DJANGO_DEBUG", "True") == "True"
+DEBUG = False 
 
 ALLOWED_HOSTS = ["*"]
 
@@ -109,7 +109,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
